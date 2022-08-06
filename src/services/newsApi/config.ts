db0826915902cb471b9334271e1d38856104c28f
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-08-06 20:14:42 -0300

    chore: add var

--- src/services/newsApi/config.ts
+++ src/services/newsApi/config.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import {KEY_NEWS_API} from "../../const/env"
+const tokem = KEY_NEWS_API || process.env.KEY_NEWS_API
 export const api = axios.create({
     baseURL: "https://newsapi.org/v2",
     headers: {
-        Authorization: KEY_NEWS_API,
+        Authorization: tokem,
     }
 
 })
